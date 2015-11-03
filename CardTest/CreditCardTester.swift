54a5f1e248120a1cf928960885b3cbf0475251af
William Berger <willerikberger@gmail.com>
2015-11-04 09:34:59 +1300

    Update syntax to use Swift 2.1

--- CardTest/CreditCardTester.swift
+++ CardTest/CreditCardTester.swift
@@ -18,34 +18,34 @@ class CreditCardTester {
     func isValidCardType(cardNumber: String) -> Bool{
         
         //AMEX -- starts with 34 or 37 -- must have length of 15 to be valid
-        if((cardNumber.rangeOfString("^(34|37)", options: .RegularExpressionSearch)) != nil && contains(acceptedCardTypes, "AMEX")){
-            return 15 == count(cardNumber)
+        if((cardNumber.rangeOfString("^(34|37)", options: .RegularExpressionSearch)) != nil && acceptedCardTypes.contains("AMEX")){
+            return 15 == cardNumber.characters.count
         }
         
         //MasterCard -- starts with 51 through 55 -- must have length of 16 to be valid
-        if((cardNumber.rangeOfString("^(51|52|53|54|55)", options: .RegularExpressionSearch)) != nil && contains(acceptedCardTypes, "MasterCard")){
-            return 16 == count(cardNumber)
+        if((cardNumber.rangeOfString("^(51|52|53|54|55)", options: .RegularExpressionSearch)) != nil && acceptedCardTypes.contains("MasterCard")){
+            return 16 == cardNumber.characters.count
         }
         
         // VISA -- starts with 4 -- must have a length of 13 or 16 to be valid
-        if((cardNumber.rangeOfString("^(4)", options: .RegularExpressionSearch)) != nil && contains(acceptedCardTypes, "VISA")){
-            return 16 == count(cardNumber) || 13 == count(cardNumber)
+        if((cardNumber.rangeOfString("^(4)", options: .RegularExpressionSearch)) != nil && acceptedCardTypes.contains("VISA")){
+            return 16 == cardNumber.characters.count || 13 == cardNumber.characters.count
         }
         
         // Diners Club -- starts with 300-305, 36 or 38 -- must have a length of 14 to be valid
-        if((cardNumber.rangeOfString("^(300|301|302|303|304|305|36|38)", options: .RegularExpressionSearch)) != nil && contains(acceptedCardTypes, "DinersClub")){
-            return 14 == count(cardNumber)
+        if((cardNumber.rangeOfString("^(300|301|302|303|304|305|36|38)", options: .RegularExpressionSearch)) != nil && acceptedCardTypes.contains("DinersClub")){
+            return 14 == cardNumber.characters.count
         }
         
         // Card type wasn't recognised, provided Unknown is in the CardTypes property, then
         // return true, otherwise return false.
-        return contains(acceptedCardTypes, "Unknown")
+        return acceptedCardTypes.contains("Unknown")
         
     }
     
     // Determines what type of credit card the given string represents based on what numbers it starts with
     func getCardType(cardNumber: String) -> String{
-        return ""
+        return "AMEX"
     }
     
 }
